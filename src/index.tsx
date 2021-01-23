ca2715ea9be8367c392f93a52501627310caca49
Lovepreet <slovepreet796@gmail.com>
2021-01-23 17:12:40 +0530

    Removed App tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Components from "./Components";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Components />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
