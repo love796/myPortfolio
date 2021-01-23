37232d0400971ede1ccfe6ef1c5f2adb47f98729
Lovepreet <slovepreet796@gmail.com>
2021-01-23 22:19:32 +0530

    Added types for all data used

---
+++ src/type.d.ts
@@ -0,0 +1,38 @@
+type Info = {
+	name: String;
+	occupation: String;
+	image: String;
+	summary: String;
+	resume: String;
+	phone: String;
+	email: String;
+	website: String;
+	location: String;
+};
+
+type Experience = {
+	date: String;
+	name: String;
+	title: String;
+	link: String;
+	description: String;
+};
+
+type Education = {
+	universityName: String;
+	period: String;
+	degree: String;
+	description: String;
+};
+
+type Skill = {
+	name: String;
+	value: Number;
+};
+
+type Project = {
+	name: String;
+	description: String;
+	role: String;
+	link: String;
+};
